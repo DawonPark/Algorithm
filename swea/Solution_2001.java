ceac4908a3d3a8490a1451ab873bee76138e5449
DawonPark <dawon950@gmail.com>
2021-08-05 19:39:23 +0900

    2021-08-05 upload

--- swea/Solution_2001.java
+++ swea/Solution_2001.java
@@ -20,14 +20,15 @@ public class Solution_2001 {
 			}
 			int x=0;
 			int y=0;
+			// m*m 을 칠 수 있는 횟수는(n-m+1)*(n-m+1)
 			for(int i=0; i<(n-m+1)*(n-m+1); i++) {
-				for(int r=0; r<m; r++) {
+				for(int r=0; r<m; r++) { // x 와 y를 이용해서 파리채를 이동
 					for(int c=0; c<m; c++) {
 						temp+=area[r+x][c+y];
 					}
 				}
-				y++;
-				if(y>(n-m)) {
+				y++; // 오른쪽으로 파리채를 이동시킴
+				if(y>(n-m)) { // y가 n-m 보다 커지면 배열에서 파리채가 나가고, 한칸 내린 왼쪽 끝에서 다시가야함.
 					y=0;
 					x++;
 				}
