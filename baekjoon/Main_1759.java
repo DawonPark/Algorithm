66303435e64b2c766f24e17124888d6615210b8f
DawonPark <dawon950@gmail.com>
2021-08-23 19:00:17 +0900

    2021-08-23

---
+++ baekjoon/Main_1759.java
@@ -0,0 +1,53 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+/*
+ * 백준 1759 암호만들기
+ * 
+ * sort를 한 후 -> C만큼의 순열을 만들고 자음 모음 체크를 해줌
+ */
+public class Main_1759 {
+	static char[] encode , decode;
+	static int L,C;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		L = sc.nextInt();
+		C = sc.nextInt();
+		sc.nextLine();
+		String msg = sc.nextLine().replace(" ","");
+		encode = msg.toCharArray();
+		Arrays.sort(encode);
+		decode = new char[C];
+		solve(0,0);
+		sc.close();
+	}
+	
+	static void solve(int cnt, int start) {
+		if(cnt==L) {
+			int check1 =0; 
+			int check2 =0;
+			
+			for(int i=0; i<L; i++) {
+				char ch=decode[i];
+				if(ch== 'a' || ch== 'e' ||ch== 'i'|| ch== 'o' || ch== 'u' ) check1++; // 최소 한개의 모음
+				else check2++; // 자음 카운트
+			}
+			if(check1 <1 || check2 <2) return;
+			
+			for(int i=0; i<L; i++) {
+				System.out.print(decode[i]);
+			}
+			System.out.println();
+			return;
+		}
+	
+		for(int i= start; i<C; i++) {
+				decode[cnt]= encode[i];
+				solve(cnt+1,i+1);
+		}
+		
+	}
+	
+}
---
+++ baekjoon/Main_4963.java
@@ -0,0 +1,63 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+/*
+ * 백준 4963 섬의 개수
+ * 
+ * -> 1인곳을 찾아 연결된 부분을 다 0으로 해주고 나오는 재귀함수 구현 1하나만 있어도 섬이기때문에 들어갈때 count를 올려줌
+ */
+
+public class Main_4963 {
+	static int w, h;
+	static int[][] map;
+	static int[] dx = {-1,-1,-1,0,0,1,1,1};
+	static int[] dy = {-1,0,1,-1,1,-1,0,1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			String data[] = br.readLine().split(" ");
+			 w = Integer.parseInt(data[0]);  h = Integer.parseInt(data[1]);
+			if(w==0 && h ==0) break;
+			map = new int[h][w];
+			for(int i=0; i<h; i++) {
+				data= br.readLine().split(" ");
+				for(int j=0; j<w; j++) {
+					map[i][j] = Integer.parseInt(data[j]);
+				}
+			}
+			int ans =0;
+			for(int i=0; i<h; i++) {
+				for(int j=0; j<w; j++) {
+					if(map[i][j]== 1) {
+						ans++;
+						dfs(i,j);
+					}
+				}
+			}
+			System.out.println(ans);
+		}
+	}
+	
+	static void dfs(int x, int y) {
+		map[x][y] =0;
+		
+		for(int d=0; d<8; d++) {
+			int nx = x +dx[d];
+			int ny = y +dy[d];
+			if(nx<0 || ny<0|| nx>=h || ny>=w) continue;
+			
+			if(map[nx][ny] == 1) {
+				dfs(nx,ny);
+			}
+			
+		}
+		
+		
+	}
+	
+	
+}
