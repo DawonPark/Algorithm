0269c0b6f0feb74ffdc0df468d48f38cd45446f8
DawonPark <dawon950@gmail.com>
2021-09-26 14:01:41 +0900

    2021-09-26

---
+++ programmers/정렬/H_Index.java
@@ -0,0 +1,18 @@
+package programmers.정렬;
+import java.util.*;
+
+public class H_Index {
+	  public int solution(int[] citations) {
+	        Arrays.sort(citations);
+	        int answer = 0;
+	        
+	        for(int i=0; i<citations.length; i++){
+	            if(citations[i]>=citations.length-i && citations[i]>=i ) {
+	                answer = citations.length-i;
+	                break;
+	            }
+	        }
+	    
+	        return answer;
+	    }
+}
