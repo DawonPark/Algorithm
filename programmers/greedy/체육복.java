86728fe9e4d9ca494e936334a6b6a1aadfcb66c7
DawonPark <dawon950@gmail.com>
2021-10-13 20:27:25 +0900

    2021-10-13

---
+++ programmers/greedy/체육복.java
@@ -0,0 +1,26 @@
+package programmers.greedy;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] people = new int[n];
+        int answer = n;
+ 
+        for (int i : lost) 
+            people[i-1]--;
+        for (int i : reserve) 
+            people[i-1]++;
+ 
+        for (int i = 0; i < people.length; i++) {
+            if(people[i] == -1) {
+                if(i-1>=0 && people[i-1] == 1) { // 바로 왼쪽인지와 체육복 여벌이 있는지 체크
+                    people[i]++;
+                    people[i-1]--;
+                }else if(i+1< people.length && people[i+1] == 1) {
+                    people[i]++;
+                    people[i+1]--;
+                }else answer--;
+            }
+        }
+        return answer;
+    }
+}
