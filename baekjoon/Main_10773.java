266a47595f44ae1af38eec416565df9d419bbe69
DawonPark <dawon950@gmail.com>
2021-10-18 20:22:18 +0900

    2021-10-18

---
+++ baekjoon/Main_10773.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main_10773 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int K = sc.nextInt();
+		Stack<Integer> stack = new Stack<>();
+		
+		for(int i=0; i<K; i++) {
+			int num = sc.nextInt();
+			if(num == 0) {
+				stack.pop();
+			}else {
+				stack.push(num);
+			}
+		}
+		int ans =0;
+		while(!stack.isEmpty()) {
+			ans += stack.pop();
+		}
+		System.out.println(ans);
+		sc.close();
+	}
+}
