9f5aea01298498edf508effc5768ae6d440b4c79
DawonPark <dawon950@gmail.com>
2021-11-09 21:30:18 +0900

    2021-11-09

--- baekjoon/Main_2156.java
+++ baekjoon/Main_2156.java
@@ -1,5 +1,19 @@
 package baekjoon;
 
-public class Main_2156 {
+import java.util.Scanner;
 
+public class Main_2156 {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] wines = new int[N];
+		boolean[] check = new boolean[N];
+		for(int i = 0; i<N; i++) {
+			wines[i] = sc.nextInt();
+		}
+		while(true) {
+
+		}
+		
+	}
 }
