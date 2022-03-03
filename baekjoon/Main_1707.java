18c25167f9b82d0bc7044259e254f5679068b2b4
DESKTOP-KVCQHCD\SSAFY <dawon950@gmail.com>
2022-03-03 21:34:21 +0900

    2022-03-03

---
+++ baekjoon/Main_1707.java
@@ -0,0 +1,68 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main_1707 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int K = Integer.parseInt(br.readLine());
+		
+outer: 	for(int t = 0; t<K; t++) {
+			String[] data= br.readLine().split(" ");
+			int V = Integer.parseInt(data[0]);
+			int E = Integer.parseInt(data[1]);
+			int[] isVisited = new int[V+1];
+			
+			ArrayList<Integer>[] list = new ArrayList[V+1];
+			
+			for(int i=0; i<=V; i++) {
+				list[i] = new ArrayList<Integer>();
+			}
+			
+			for(int i=0; i<E; i++) {
+				data= br.readLine().split(" ");
+				int start = Integer.parseInt(data[0]);
+				int end = Integer.parseInt(data[1]);
+				
+				list[start].add(end);
+				list[end].add(start);
+			}
+			
+			Queue<Integer> queue = new LinkedList<>();
+			
+			for(int i=1; i<=V; i++) {
+				if(isVisited[i] == 0) {
+					queue.add(i);
+					isVisited[i]= 1;
+				}
+				
+				while(!queue.isEmpty()) {
+					int start = queue.poll();
+					
+					for(int j=0; j<list[start].size(); j++) {
+						if(isVisited[list[start].get(j)]==0) {
+							queue.add(list[start].get(j));
+						}
+						if(isVisited[list[start].get(j)] == isVisited[start]) {
+							System.out.println("NO");
+							continue outer;
+						}
+						if(isVisited[start] == 1 && isVisited[list[start].get(j)] == 0)
+							isVisited[list[start].get(j)] = 2;
+						else if(isVisited[start] == 2 && isVisited[list[start].get(j)] == 0)
+							isVisited[list[start].get(j)] = 1;
+					}
+				}
+			}
+			System.out.println("YES");
+			
+			
+		}
+		
+	}
+}
---
+++ baekjoon/Main_9663.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main_9663 {
+	static int[] arr;
+	static int N;
+	static int ans;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		arr = new int[N];
+		ans = 0;
+		solve(0);
+		System.out.println(ans);
+		sc.close();
+	}
+
+	private static void solve(int row) {
+		if(row == N) {
+			ans++;
+			return;
+		}
+		for(int i=0; i<N; i++) {
+			arr[row] = i;
+			if(go(row)) {
+				solve(row+1);
+			}
+		}
+	}
+
+	private static boolean go(int row) {
+		for(int i=0; i<row; i++) {
+			if(arr[row] == arr[i])
+			return false;
+			if(Math.abs(row-i) == Math.abs(arr[row]- arr[i]))
+			return false;
+		}
+		return true;
+	}
+}
