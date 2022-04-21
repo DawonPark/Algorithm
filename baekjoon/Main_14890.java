53900fe7ef1dbe6d1822d4edb7fb1419f411d37f
박다원 <dawon950@gmail.com>
2022-04-22 01:10:09 +0900

    2022-04-22

--- baekjoon/Main_14890.java
+++ baekjoon/Main_14890.java
@@ -1,5 +1,6 @@
 package baekjoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main_14890 {
@@ -20,29 +21,56 @@ public class Main_14890 {
 			goCol(0,i);
 			goRow(i,0);
 		}
-		
+		System.out.println(ans);
 		
 	}
 	private static void goCol(int x, int y) {
-		
-		
-		
-		ans++;
-	}
-	private static void goRow(int x, int y) {
-		int current = map[x][y];
-		boolean check = false;
-		for(int i=1; i<N; i++) {
-			if(current > map[x][i]) {
-				if(!check && i+L <N ) {
-					for(int j=1; j<=L; j++) {
-						if(map[x][i]!=map[x][i+j]) return;
-					}
+		boolean[] flags = new boolean[N];
+		for(int i=0; i<N-1; i++) {
+			int diff = map[i][y] - map[i+1][y];
+			if(Math.abs(diff) > 1) return;
+			if(diff == 0) continue;
+			if(diff == 1) {
+				for(int j=1; j<=L; j++) {
+					if(i+j >=N || flags[i+j]) return;
+					if(map[i][y] - 1 != map[i+j][y]) return;
+					flags[i+j] = true;
+				}
+			}else if(diff == -1) {
+				for(int j=0; j<L; j++) {
+					if(i-j < 0 || flags[i-j]) return;
+					if(map[i][y] != map[i-j][y]) return;
+					flags[i-j] = true;
 				}
 			}
 		}
 		
-		
+//		System.out.println("col: " + y);
 		ans++;
 	}
+	private static void goRow(int x, int y) {
+		boolean[] flags = new boolean[N];
+		for(int i=0; i<N-1; i++) {
+			int diff = map[x][i] - map[x][i+1];
+			if(Math.abs(diff) > 1) return;
+			if(diff == 0) continue;
+			if(diff == 1) {
+				for(int j=1; j<=L; j++) {
+					if(i+j >=N || flags[i+j]) return;
+					if(map[x][i] - 1 != map[x][i+j]) return;
+					flags[i+j] = true;
+				}
+			}else if(diff == -1) {
+				for(int j=0; j<L; j++) {
+					if(i-j < 0 || flags[i-j]) return;
+					if(map[x][i] != map[x][i-j]) return;
+					flags[i-j] = true;
+				}
+			}
+		}
+		
+//		System.out.println("row: " + x);
+		ans++;
+	}	
 }
+
