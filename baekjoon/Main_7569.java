380fb51274f1f8c331686aa16d737f5ebe665210
박다원 <dawon950@gmail.com>
2022-04-23 18:49:08 +0900

    2022-04-23

---
+++ baekjoon/Main_7569.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main_7569 {
+	static int[] dx = {0,0,-1,1};
+	static int[] dy = {-1,1,0,0};
+	static int[][] map;
+ 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int M = sc.nextInt();
+		int N = sc.nextInt();
+		int H = sc.nextInt();
+		
+	}
+}
