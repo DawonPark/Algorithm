d71697399c3fad3df1005e6c43966e5039d8d0bf
DawonPark <dawon950@gmail.com>
2022-05-25 17:52:18 +0900

    2022-05-25

---
+++ JavaScript/baekjoon/후위표기식2.js
@@ -0,0 +1,35 @@
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+let input = fs.readFileSync(filePath).toString().replace(/\r/g, "").split("\n");
+const stack = [];
+const N = Number(input[0]);
+const num = [];
+console.log(input);
+for (let i = 2; i < 2 + N; i++) {
+  num[i - 2] = Number(input[i]);
+}
+
+for (let i = 0; i < input[1].trim().length; i++) {
+  if ("A" <= input[1].charAt(i) && input[1].charAt(i) <= "Z")
+    stack.push(num[input[1].charAt(i).charCodeAt(0) - "A".charCodeAt(0)]);
+  else {
+    let second = stack.pop();
+    let first = stack.pop();
+    switch (input[1].charAt(i)) {
+      case "+":
+        stack.push(first + second);
+        break;
+      case "-":
+        stack.push(first - second);
+        break;
+      case "*":
+        stack.push(first * second);
+        break;
+      case "/":
+        stack.push(first / second);
+        break;
+    }
+  }
+}
+console.log(stack[0].toFixed(2));
